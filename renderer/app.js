5f5384c79802b483ba50c05bd48dc88c4bc6fd35
adrisanchu <adrian.sanchez@constant-urban.eu>
2022-11-10 00:22:56 +0100

    initial setup: show and hide modal + listen to add item event + focus on input + submit on enter

--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,32 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// DOM nodes
+let showModal = document.getElementById('show-modal'),
+	closeModal = document.getElementById('close-modal'),
+	modal = document.getElementById('modal'),
+	addItem = document.getElementById('add-item'),
+	itemUrl = document.getElementById('url');
+
+// show modal
+showModal.addEventListener('click', (e) => {
+	modal.style.display = 'flex';
+	itemUrl.focus();
+});
+
+// hide modal
+closeModal.addEventListener('click', (e) => {
+	modal.style.display = 'none';
+});
+
+// add a new item
+addItem.addEventListener('click', (e) => {
+	// check if url exists
+	if (itemUrl.value) {
+		console.log(itemUrl.value);
+		itemUrl.value = '';
+		closeModal.click();
+	}
+});
+
+// Listen for keyboard submit
+itemUrl.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') addItem.click();
+});
