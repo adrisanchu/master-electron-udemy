65eb21d8f0de4f55f30acba913d20142a9ee9413
adrisanchu <adrian.sanchez@constant-urban.eu>
2022-11-12 10:40:21 +0100

    add web items as DOM nodes in the renderer process

--- renderer/app.js
+++ renderer/app.js
@@ -1,4 +1,5 @@
 const { ipcRenderer } = require('electron');
+const items = require('./items');
 
 // DOM nodes
 let showModal = document.getElementById('show-modal'),
@@ -48,7 +49,9 @@ addItem.addEventListener('click', (e) => {
 
 // Listen for new item from main process
 ipcRenderer.on('new-item-success', (e, newItem) => {
-	console.log(newItem);
+	
+	// Add new item to "items" node
+	items.addItem(newItem);
 
   // Enable buttons
   toggleModalButtons();
---
+++ renderer/items.js
@@ -0,0 +1,18 @@
+// DOM nodes
+let items = document.getElementById('items');
+
+const addItem = (item) => {
+	// Create a new DOM node
+	let itemNode = document.createElement('div');
+
+	// Assign "read-item" class
+	itemNode.setAttribute('class', 'read-item');
+
+	// Add inner HTML
+	itemNode.innerHTML = `<img src="${item.screenshot}"><h2>${item.title}</h2>`;
+
+	// Append new node to "items"
+	items.appendChild(itemNode);
+};
+
+module.exports = { addItem };
