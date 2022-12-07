606c64234f4097111bb3997def236376ed41a60e
adrisanchu <adrian.sanchez@constant-urban.eu>
2022-12-07 20:11:22 +0100

    menu: use roles to add default Electron native options to our menu

--- menu.js
+++ menu.js
@@ -1,9 +1,34 @@
-const { Menu } = require('electron');
+const { Menu, shell } = require('electron');
 
 // Module function to create main app menu
 module.exports = () => {
 	// Menu temlate
-	let template = [];
+	let template = [
+		{
+			label: 'Items',
+			submenu: [],
+		},
+		{
+			role: 'editMenu',
+		},
+		{
+			role: 'windowMenu',
+		},
+		{
+			role: 'help',
+			submenu: [
+				{
+					label: 'Learn more',
+					click: () => {
+						shell.openExternal('https://github.com/adrisanchu');
+					},
+				},
+			],
+		},
+	];
+
+	// Create Mac app menu
+	if (process.platform === 'darwin') template.unshift({ role: 'appMenu' });
 
 	// Build menu
 	let menu = Menu.buildFromTemplate(template);
